e93a7538e0bed3100ff3352433b9550a36498eed
Jahanvi Sharma <janviisharma.183@gmail.com>
2026-02-19 13:54:37 +0530

    updated

--- index4a.js
+++ index4a.js
@@ -1,9 +1,13 @@
-// import express from "express"
-// const app= express();
-// app.listen(8070,()=>{
-//     console.log("Server Started");
-// });
-
+import express from "express"
+const app= express();
+app.listen(8070,()=>{
+    console.log("Server Started");
+});
+app.get("/:id/:id1/:id2/:id3", (req, res) => {
+    console.log(req.url);
+    console.log(req.params);
+    res.send("hello students");
+});
 
 // app.get("/:id/:id1", (req, res) => {
 //     console.log(req.url);
@@ -15,20 +19,27 @@
 // });
 
 
+
+
+// app.get("/x/y", (req, res) => {
+//     res.send("Good morning");
+// });
+
+
+
+
 //x/4/y/5/z/4
-import express from "express"
-const app= express();
-app.listen(8070,()=>{
-    console.log("Server Started");
-});
-
-
-app.get("/x/:id/y/:id1/z/:id2", (req, res) => {
-    console.log(req.url);
-    console.log(req.params);
-    const id = Number(req.params.id);
-    const id1 = Number(req.params.id1);
-    const id2 = Number(req.params.id2);
-    const sum = id + id1 + id2;
-    res.send(`Sum is: ${sum}`);
-});
+// import express from "express"
+// const app= express();
+// app.listen(8070,()=>{
+//     console.log("Server Started");
+// });
+// app.get("/x/:id/y/:id1/z/:id2", (req, res) => {
+//     console.log(req.url);
+//     console.log(req.params);
+//     const id = Number(req.params.id);
+//     const id1 = Number(req.params.id1);
+//     const id2 = Number(req.params.id2);
+//     const sum = id + id1 + id2;
+//     res.send(`Sum is: ${sum}`);
+// });
