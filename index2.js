747d57a06c190c73d414ddd34e84e806f1eee022
Jahanvi Sharma <janviisharma.183@gmail.com>
2026-02-19 11:29:48 +0530

    updated

---
+++ index2.js
@@ -0,0 +1,9 @@
+import express from 'express'
+const app=express();
+app.listen(8070,()=>{
+    console.log("Srevr Started");
+});
+app.get("/",(req,res)=>{
+    console.log(req.url)
+    res.send("response for server from /")
+})
